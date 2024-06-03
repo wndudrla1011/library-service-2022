64f21e8be2623d90a856e412bc02c4b0bd2ace18
wndudrla1011 <wndudrla1011@naver.com>
2024-06-03 19:24:35 +0900

    off spring security

--- src/main/java/com/rootable/libraryservice2022/LibraryService2022Application.java
+++ src/main/java/com/rootable/libraryservice2022/LibraryService2022Application.java
@@ -2,12 +2,13 @@ package com.rootable.libraryservice2022;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing //Jpa Auditing 활성화
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class LibraryService2022Application {
 
 	public static void main(String[] args) {
