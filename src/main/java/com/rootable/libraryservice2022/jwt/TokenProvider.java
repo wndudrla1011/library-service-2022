254c85547a0b77aab04bf753a42e0c5cbd377de7
wndudrla1011 <wndudrla1011@naver.com>
2024-06-04 16:39:11 +0900

    added validation logic for token

--- src/main/java/com/rootable/libraryservice2022/jwt/TokenProvider.java
+++ src/main/java/com/rootable/libraryservice2022/jwt/TokenProvider.java
@@ -1,12 +1,18 @@
 package com.rootable.libraryservice2022.jwt;
 
+import io.jsonwebtoken.security.SecurityException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -24,6 +30,28 @@ public class TokenProvider {
         this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;
     }
 
+    //토큰 검증
+    public boolean verify(String token) {
 
+        try {
+            //토큰 복호화
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+
+        return false;
+
+    }
 
 }
