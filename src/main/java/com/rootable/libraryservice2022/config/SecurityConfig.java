c98258708a742c91598769a12272c6e8d0308953
wndudrla1011 <wndudrla1011@naver.com>
2024-06-04 12:29:14 +0900

    added passwordEncoder

---
+++ src/main/java/com/rootable/libraryservice2022/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.rootable.libraryservice2022.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
