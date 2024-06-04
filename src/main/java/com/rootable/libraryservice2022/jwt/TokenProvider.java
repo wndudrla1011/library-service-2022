fe589fb5a805eb3faf79176a164cbaf6fd5019e8
wndudrla1011 <wndudrla1011@naver.com>
2024-06-04 15:37:18 +0900

    added logic creation secret key

--- src/main/java/com/rootable/libraryservice2022/jwt/TokenProvider.java
+++ src/main/java/com/rootable/libraryservice2022/jwt/TokenProvider.java
@@ -1,11 +1,28 @@
 package com.rootable.libraryservice2022.jwt;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.io.Decoders;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
 
 @Slf4j
 @Component
 public class TokenProvider {
+
+    private static final String AUTHORITIES_KEY = "auth";
+    private final long tokenValidityInMilliseconds;
+    private final Key key;
+
+    //토큰 생성에 사용될 비밀키 생성
+    public TokenProvider(@Value("${jwt.secret}") String secret,
+                         @Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds) {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;
+    }
 
 
 
