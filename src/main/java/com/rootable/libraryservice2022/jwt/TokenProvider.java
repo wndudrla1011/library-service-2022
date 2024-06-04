858196aee1f1268a3594063b7caf23e5eb1d4d7d
wndudrla1011 <wndudrla1011@naver.com>
2024-06-04 12:42:10 +0900

    created TokenProvider

---
+++ src/main/java/com/rootable/libraryservice2022/jwt/TokenProvider.java
@@ -0,0 +1,12 @@
+package com.rootable.libraryservice2022.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class TokenProvider {
+
+
+
+}
