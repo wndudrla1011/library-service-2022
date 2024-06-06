b15ab2694588bda2647f61b3bd05b60349f9501f
wndudrla1011 <wndudrla1011@naver.com>
2024-06-06 15:27:56 +0900

    edited IllegalArgumentException -> JwtException

--- src/main/java/com/rootable/libraryservice2022/jwt/TokenProvider.java
+++ src/main/java/com/rootable/libraryservice2022/jwt/TokenProvider.java
@@ -61,7 +61,7 @@ public class TokenProvider {
             log.info("만료된 JWT 토큰입니다.");
         } catch (UnsupportedJwtException e) {
             log.info("지원되지 않는 JWT 토큰입니다.");
-        } catch (IllegalArgumentException e) {
+        } catch (JwtException e) {
             log.info("JWT 토큰이 잘못되었습니다.");
         }
 
