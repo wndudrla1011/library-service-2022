2c075dccaf016e801527b71e034e17303271733f
wndudrla1011 <wndudrla1011@naver.com>
2024-06-07 17:54:41 +0900

    rollback project to session type - SSR 서버에 JWT 적용으로 인해 득보다 실이 크다고 판단

--- src/main/java/com/rootable/libraryservice2022/config/WebConfig.java
+++ src/main/java/com/rootable/libraryservice2022/config/WebConfig.java
@@ -1,9 +1,7 @@
 package com.rootable.libraryservice2022.config;
 
-import com.rootable.libraryservice2022.web.argumentresolver.JwtLoginResolver;
 import com.rootable.libraryservice2022.web.argumentresolver.LoginMemberArgumentResolver;
 import com.rootable.libraryservice2022.web.interceptor.AuthInterceptor;
-import com.rootable.libraryservice2022.web.interceptor.JwtLoginCheckInterceptor;
 import com.rootable.libraryservice2022.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -17,8 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LoginCheckInterceptor())
-        registry.addInterceptor(new JwtLoginCheckInterceptor())
+        registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error/**", "/js/**");
@@ -32,8 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-//        resolvers.add(new LoginMemberArgumentResolver());
-        resolvers.add(new JwtLoginResolver());
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 
 }
